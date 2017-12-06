a277c914be1ce348d1572ecd5f854daeb590af04
Remi PINOIT <rpinoit@e2r10p14.42.fr>
2017-12-06 14:00:34 +0100

    add gnl

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpinoit <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/04 09:38:07 by rpinoit           #+#    #+#             */
+/*   Updated: 2017/12/05 18:48:53 by rpinoit          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <unistd.h>
+# define BUFF_SIZE 32
+
+int	get_next_line(const int fd, char **line);
+
+#endif
