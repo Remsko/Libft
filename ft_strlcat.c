1d776ff84a06c708e292987b50e170b83aecf73a
Remi <pinoit.remi@gmail.com>
2018-03-18 23:08:54 +0100

    update : clear / optimize code

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -14,23 +14,18 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t i;
-	size_t j;
+	size_t dst_len;
+	size_t src_len;
 
-	i = 0;
-	j = 0;
-	if (size != 0)
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	src_len -= src_len - size;
+	if (src_len < size - dst_len)
+		ft_strncpy(dst + dst_len, src, src_len + 1);
+	else if (src_len > size - dst_len)
 	{
-		while (dst[i] != '\0' && i < size)
-			i++;
-		j = i;
-		while (src[i - j] != '\0' && (i < size - 1))
-		{
-			dst[i] = src[i - j];
-			i++;
-		}
-		if (i < size)
-			dst[i] = '\0';
+		ft_strncpy(dst + dst_len, src, size - 1 - dst_len);
+		dst[size - 1] = '\0';
 	}
-	return (j + ft_strlen(src));
+	return (src_len + dst_len);
 }
