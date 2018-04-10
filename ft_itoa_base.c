1d00df7e95a5895ede7e7c49d96a28bd75ab8221
Remi PINOIT <rpinoit@e2r10p14.42.fr>
2018-04-10 16:47:24 +0200

    noob fix BASE 10

--- ft_itoa_base.c
+++ ft_itoa_base.c
@@ -6,7 +6,7 @@
 /*   By: rpinoit <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/16 15:29:47 by rpinoit           #+#    #+#             */
-/*   Updated: 2018/04/10 16:45:33 by rpinoit          ###   ########.fr       */
+/*   Updated: 2018/04/10 16:47:02 by rpinoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_itoa_base(int value, int base)
 
 	if (base < 2 || base > 16)
 		return (NULL);
-	len = (value == 0 || (value < 0 && base == 0));
+	len = (value == 0 || (value < 0 && base == 10));
 	tmp = value;
 	while (++len && tmp)
 		tmp /= base;
