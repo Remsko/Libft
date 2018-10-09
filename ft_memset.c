e5f6b93f9557a28aafdb1751c72f87c8b71954ee
Remsko <pinoit.remi@gmail.com>
2018-10-09 22:30:38 +0200

    tested memset

--- ft_memset.c
+++ ft_memset.c
@@ -3,66 +3,78 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rpinoit <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: rpinoit <rpinoit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 17:57:37 by rpinoit           #+#    #+#             */
-/*   Updated: 2018/03/16 11:13:57 by rpinoit          ###   ########.fr       */
+/*   Updated: 2018/10/09 22:14:56 by rpinoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *dst, int src, size_t len)
+#define	op_t	unsigned long int
+#define OPSIZ	(sizeof(op_t))
+
+void *memset (void *dstpp, int c, size_t len)
 {
-	unsigned int	c;
-	long int	dst_ptr;
+  long int dstp = (long int) dstpp;
 
-	c = src;
-	dst_ptr = (long int)dst;
-	if (len >= 8)
-	{
-		size_t	xlen;
-		unsigned long cccc;
+  if (len >= 8)
+    {
+      size_t xlen;
+      op_t cccc;
 
-		cccc = (unsigned char)c;
-		cccc |= cccc << 8;
-		cccc |= cccc << 16;
-		cccc |= cccc << 32;
-		while (dst_ptr % sizeof(long int))
-		{
-			((unsigned char *)dst_ptr)[0] = c;
-			dst_ptr += 1;
-			len -= 1;
-		}
-		xlen = len / (sizeof(long int) * 8);
-		while (xlen > 0)
-		{
-			((long int *)dst_ptr)[0] = cccc;
-			((long int *)dst_ptr)[1] = cccc;
-			((long int *)dst_ptr)[2] = cccc;
-			((long int *)dst_ptr)[3] = cccc;
-			((long int *)dst_ptr)[4] = cccc;
-			((long int *)dst_ptr)[5] = cccc;
-			((long int *)dst_ptr)[6] = cccc;
-			((long int *)dst_ptr)[7] = cccc;
-			dst_ptr += 8 * sizeof(long int);
-			xlen -= 1;
-		}
-		len %= sizeof(long int);
-		xlen = len / sizeof(long int);
-		while (xlen > 0)
-		{
-			((long int *)dst_ptr)[0] = c;
-                        dst_ptr += sizeof(long int);
-                        len -= 1;
-		}
-		len %= sizeof(long int);
-	}
-	while (len > 0)
+      cccc = (unsigned char) c;
+      cccc |= cccc << 8;
+      cccc |= cccc << 16;
+    //  if (OPSIZ > 4)
+	/* Do the shift in two steps to avoid warning if long has 32 bits.  */
+	//cccc |= (cccc << 16) << 16;
+
+      /* There are at least some bytes to set.
+	 No need to test for LEN == 0 in this alignment loop.  */
+      while (dstp % OPSIZ != 0)
 	{
-		((unsigned char *)dst_ptr)[0] = c;
-		dst_ptr += 1;
-		len -= 1;
+	  ((unsigned char *) dstp)[0] = c;
+	  dstp += 1;
+	  len -= 1;
 	}
-	return (dst);
+
+      /* Write 8 `op_t' per iteration until less than 8 `op_t' remain.  */
+      xlen = len / (OPSIZ * 8);
+      while (xlen > 0)
+	{
+	  ((op_t *) dstp)[0] = cccc;
+	  ((op_t *) dstp)[1] = cccc;
+	  ((op_t *) dstp)[2] = cccc;
+	  ((op_t *) dstp)[3] = cccc;
+	  ((op_t *) dstp)[4] = cccc;
+	  ((op_t *) dstp)[5] = cccc;
+	  ((op_t *) dstp)[6] = cccc;
+	  ((op_t *) dstp)[7] = cccc;
+	  dstp += 8 * OPSIZ;
+	  xlen -= 1;
+	}
+      len %= OPSIZ * 8;
+
+      /* Write 1 `op_t' per iteration until less than OPSIZ bytes remain.  */
+      xlen = len / OPSIZ;
+      while (xlen > 0)
+	{
+	  ((op_t *) dstp)[0] = cccc;
+	  dstp += OPSIZ;
+	  xlen -= 1;
+	}
+      len %= OPSIZ;
+    }
+
+  /* Write the last few bytes.  */
+  while (len > 0)
+    {
+      ((unsigned char *) dstp)[0] = c;
+      dstp += 1;
+      len -= 1;
+    }
+
+  return dstpp;
 }
